3cd9e2bd782ec65c7766f3add51914b8f4221cbb
Pratik Poddar <pratik.phodu@gmail.com>
2013-12-24 15:18:01 +0000

    Making it private for secretpratikpoddar

--- penpatron/content-affiliate-advertising/getContentAffiliateAdvertising.php
+++ penpatron/content-affiliate-advertising/getContentAffiliateAdvertising.php
@@ -3,9 +3,15 @@ header('Content-Type: application/json');
 //$text = $_POST['text'];
 //$options = $_POST['options'];
 //$tracker = $_POST['tracker'];
+if (strpos($_SERVER['HTTP_REFERER'], "secretpratikpoddar")==FALSE) {
+	echo $_GET['jsonp_callback']."()";
+}
 
-//echo "[{'keyword': 'Columbia Business School', 'link': 'http://www.cseblog.com'}]";
-echo $_GET['jsonp_callback'] .'({"keywords":'.exec('python content-affiliate-advertising.py "'.$_GET['text'].'" '.$_GET['index']).'})';
-//echo $_GET['jsonp_callback'] .'({"keywords":[{"keyword": "Columbia Business School", "link": "http://www.cseblog.com/checking"}, {"keyword": "IBM", "link": "http://www.pratikpoddar.com"}]})';
+else {
+
+	//echo "[{'keyword': 'Columbia Business School', 'link': 'http://www.cseblog.com'}]";
+	echo $_GET['jsonp_callback'] .'({"keywords":'.exec('python content-affiliate-advertising.py "'.$_GET['text'].'" '.$_GET['index']).'})';
+	//echo $_GET['jsonp_callback'] .'({"keywords":[{"keyword": "Columbia Business School", "link": "http://www.cseblog.com/checking"}, {"keyword": "IBM", "link": "http://www.pratikpoddar.com"}]})';
+}
 ?>
 
