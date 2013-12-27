5c723189e41f4e1623e2f79fec33da67f363faad
Pratik Poddar <pratik.phodu@gmail.com>
2013-12-27 23:47:02 +0530

    Content Affiliate Advertising: NLTK NE

--- penpatron/content-affiliate-advertising/content-affiliate-advertising.py
+++ penpatron/content-affiliate-advertising/content-affiliate-advertising.py
@@ -26,33 +26,43 @@ def content_affiliate(content, index):
 def get_Content_Affliate_Advertising(content, index):
 
 	keywords = []
-	try:
-		k1 = get_Calais_Topics(content)
-	except:
-		k1 = []
+	#try:
+	#	k1 = get_Calais_Topics(content)
+	#except:
+	#	k1 = []
 	
-	try:
-		k2 = get_Content_Analysis(content)
-	except:
-		k2 = []
+	#try:
+	#	k2 = get_Content_Analysis(content)
+	#except:
+	#	k2 = []
+
+	#try:
+	#	k3 = get_Text_Concepts(content)
+	#except:
+	#	k3 = []
 
 	try:
-		k3 = get_Text_Concepts(content)
+		k4 = get_nltk_ne(content)
 	except:
-		k3 = []
+		k4 = []
+
+	#try:
+	#	keywords += map(lambda x: x['text'], k1)
+	#except:
+	#	pass
+
+	#try:
+	#	keywords += map(lambda x: x['text'], k2)
+	#except:
+	#	pass
+
+	#try:
+	#	keywords += map(lambda x: x['text'], k3)
+	#except:
+	#	pass
 
 	try:
-		keywords += map(lambda x: x['text'], k1)
-	except:
-		pass
-
-	try:
-		keywords += map(lambda x: x['text'], k2)
-	except:
-		pass
-
-	try:
-		keywords += map(lambda x: x['text'], k3)
+		keywords += map(lambda x: x['text'], k4)
 	except:
 		pass
 
