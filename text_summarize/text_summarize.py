206e9cffcd2bb6682d740b10ba6e386d90343fc1
Pratik Poddar <pratik.phodu@gmail.com>
2014-01-03 02:41:03 +0530

    Minor

--- text_summarize/text_summarize.py
+++ text_summarize/text_summarize.py
@@ -9,51 +9,90 @@ import re
 from lxml import etree
 from calais import Calais
 import nltk
+from tagger import tagger
+import pickle
+from topia.termextract import extract
 
 alchemyapi = AlchemyAPI()
 calais = Calais("rjfq8eq99bwum4fp3ncjafdw", submitter="python-calais-content-r-square")
 
 def removeNonAscii(s): return "".join(filter(lambda x: ord(x)<128, s))
 
+def get_python_tagger(text):
+
+        print('')
+        print ('## Python Tagger ##')
+	weights = pickle.load(open('text_summarize/tagger/data/dict.pkl', 'rb')) 
+	myreader = tagger.Reader()
+	mystemmer = tagger.Stemmer()
+	myrater = tagger.Rater(weights)
+	mytagger = tagger.Tagger(myreader, mystemmer, myrater)
+	tags = list(mytagger(text, 5))
+
+        responseOutput = []
+	print(tags)
+        for tag in tags:
+		try:
+	                responseOutput.append({'text': str(tag), 'freebase': get_Freebase_Meaning(str(tag)), 'source': "get_python_tagger"})
+		except Exception as e:
+			print('Error in Python Tagger: ' + str(e))
+
+        responseOutput = filter(lambda x: x['freebase'] != None, responseOutput)
+        try:
+                print(responseOutput)
+        except:
+                pass
+        return responseOutput
+	
+
 def get_nltk_ne(text):
 
 	print('')
 	print ('## NLTK NE ##')
-	tags = nltk.pos_tag(nltk.word_tokenize(text))
-	ne = nltk.ne_chunk(tags)
-	list_of_people_nes = []
-	list_of_orgs_nes = []
-	for i in range(len(ne)):
-		try:
-			if (ne[i].node=="PERSON"):
-			   	list_of_people_nes.append(' '.join(map(lambda x: x[0], ne[i].leaves())))
-		except:
-			pass
-
+        tags = nltk.pos_tag(nltk.word_tokenize(text))
+        ne = nltk.ne_chunk(tags, binary=True)
+        list_of_nes = []
+        for i in range(len(ne)):
                 try:
-                        if (ne[i].node=="ORGANIZATION"):
-                                list_of_orgs_nes.append(' '.join(map(lambda x: x[0], ne[i].leaves())))
+                        if (ne[i].node=="NE"):
+                                list_of_nes.append(' '.join(map(lambda x: x[0], ne[i].leaves())))
                 except:
                         pass
 
-	list_of_nes = []
-	list_of_nes += list_of_people_nes + list_of_orgs_nes
-	list_of_nes += map(lambda x: ' '.join(x), zip(list_of_people_nes[:-1],list_of_people_nes[1:]))
-	list_of_nes += map(lambda x: ' '.join(x), zip(list_of_orgs_nes[:-1],list_of_orgs_nes[1:]))
-	
-	list_of_nes = filter(lambda x: len(x)>4, list_of_nes)
-	
+        list_of_nes = filter(lambda x: len(x)>4, list_of_nes)
+
+        responseOutput = []
+
+        for ne in list_of_nes:
+                responseOutput.append({'text': ne, 'freebase': get_Freebase_Meaning(ne), 'source': "get_nltk_ne"})
+
+        responseOutput = filter(lambda x: x['freebase'] != None, responseOutput)
+        try:
+                print(responseOutput)
+        except:
+                pass
+        return responseOutput
+
+def get_topia_termextract(text):
+
+        print('')
+        print ('## Topia Termextract ##')
+	extractor = extract.TermExtractor()
+	tags = extractor(text)
+	tags = map(lambda x: x[0], filter(lambda x: len(x[0])>6, tags))
+
 	responseOutput = []
 
-	for ne in list_of_nes:
-		responseOutput.append({'text': ne, 'freebase': get_Freebase_Meaning(ne), 'source': "get_nltk_ne"})
+	for tag in tags:
+		responseOutput.append({'text': str(tag), 'freebase': get_Freebase_Meaning(str(tag)), 'source': "get_topia_termextract"})
 
 	responseOutput = filter(lambda x: x['freebase'] != None, responseOutput)
-	try:
-		print(responseOutput)		
-	except:
-		pass
-	return responseOutput
+        try:
+                print(responseOutput)
+        except:
+                pass
+        return responseOutput
+
 
 def get_Text_Concepts(text):
 	response = alchemyapi.concepts('text', text)
@@ -67,7 +106,7 @@ def get_Text_Concepts(text):
 			responseOutput.append({'text': concept['text'], 'freebase': get_Freebase_Meaning(concept['text']), 'source': "get_Text_Concepts"})
 		return responseOutput
 	else:
-		print('Error in concept tagging call: '+ response['statusInfo'])
+		print('Error in Alchemy concept tagging call: '+ response['statusInfo'])
 		return None
 
 def get_Text_Categories(text):
@@ -84,29 +123,34 @@ def get_Text_Categories(text):
 			responseOutput = {'text': response['category'], 'freebase': get_Freebase_Meaning(response['category']), 'source': "get_Text_Categories" }
 		return responseOutput
 	else:
-		print('Error in text categorization call: '+ response['statusInfo'])
+		print('Error in Alchemy text categorization call: '+ response['statusInfo'])
 		return None
 
 def get_Content_Analysis(text):
 	
 	url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20contentanalysis.analyze%20where%20text%3D%22" + urllib.quote_plus(re.compile('\W+').sub(' ', text).strip()) + "%22&diagnostics=true"
-	root = etree.fromstring(urllib2.urlopen(url).read())
-	relevantlist = root.getchildren()[1].getchildren()
-	print('')
-        print('## Yahoo Content Analysis ##')
-	yresult = {}
-	responseOutput = []
-	yresult['yentities'] = []
-	yresult['ycategories'] = []
-	for relevantgroup in relevantlist:
-		if relevantgroup.tag == "{urn:yahoo:cap}entities":
-			yresult['yentities'] = map(lambda entity: entity.getchildren()[0].text, relevantgroup)
-			responseOutput += map(lambda x: {'text': x, 'freebase': get_Freebase_Meaning(x), 'source':'yentities'}, yresult['yentities'])
-		if relevantgroup.tag == "{urn:yahoo:cap}yctCategories":
-			yresult['ycategories'] = map(lambda category: category.text, relevantgroup.getchildren())
-			responseOutput += map(lambda x: {'text': x, 'freebase': get_Freebase_Meaning(x), 'source':'ycategories'}, yresult['ycategories'])
-	print(responseOutput)
-	return responseOutput
+
+	try:
+		root = etree.fromstring(urllib2.urlopen(url).read())
+		relevantlist = root.getchildren()[1].getchildren()
+		print('')
+	        print('## Yahoo Content Analysis ##')
+		yresult = {}
+		responseOutput = []
+		yresult['yentities'] = []
+		yresult['ycategories'] = []
+		for relevantgroup in relevantlist:
+			if relevantgroup.tag == "{urn:yahoo:cap}entities":
+				yresult['yentities'] = map(lambda entity: entity.getchildren()[0].text, relevantgroup)
+				responseOutput += map(lambda x: {'text': x, 'freebase': get_Freebase_Meaning(x), 'source':'yentities'}, yresult['yentities'])
+			if relevantgroup.tag == "{urn:yahoo:cap}yctCategories":
+				yresult['ycategories'] = map(lambda category: category.text, relevantgroup.getchildren())
+				responseOutput += map(lambda x: {'text': x, 'freebase': get_Freebase_Meaning(x), 'source':'ycategories'}, yresult['ycategories'])
+		print(responseOutput)
+		return responseOutput
+	except Exception as e:
+		print('Error in Yahoo Content Analysis: ' + str(e))
+		
 
 def get_Calais_Topics(text):
 	
@@ -119,6 +163,7 @@ def get_Calais_Topics(text):
 		calais_result.print_topics()
 		responseOutput += map(lambda x: {'text': x['name'], 'source': 'calais_topics', 'freebase': get_Freebase_Meaning(x['name'])}, calais_result.topics)
 	except Exception as e:
+		print('Error in Calais Topics Extraction ' + str(e))
 		pass
 
 	try:
@@ -126,6 +171,7 @@ def get_Calais_Topics(text):
 		calais_result.print_entities()
 		responseOutput += map(lambda x: {'text': x['name'], 'source': 'calais_entities', 'freebase': get_Freebase_Meaning(x['name'])}, calais_result.entities)
 	except Exception as e:
+		print('Error in Calais Entities Extraction ' + str(e))
 		pass
 	
 	#responseOutput = filter(lambda x: x['freebase'] != None, responseOutput)
@@ -138,17 +184,21 @@ def get_Freebase_Meaning(term):
 		term = removeNonAscii(term)
 		url = "https://www.googleapis.com/freebase/v1/search?query=" + urllib.quote_plus(term)
 		jsonResult = json.loads(urllib2.urlopen(url).read())['result']
-		if jsonResult[0]['score']>100:
-			try:
-				return {'parentnode': jsonResult[0]['notable']['name'], 'wikilink': jsonResult[0]['id']}
-			except:
+		if jsonResult:
+			if jsonResult[0]['score']>100:
 				try:
-					return {'parentnode': '', 'wikilink': jsonResult[0]['id']}
+					return {'parentnode': jsonResult[0]['notable']['name'], 'wikilink': jsonResult[0]['id']}
 				except:
-					return {'parentnode': jsonResult[0]['notable']['name'], 'wikilink': ''}
+					try:
+						return {'parentnode': '', 'wikilink': jsonResult[0]['id']}
+					except:
+						return {'parentnode': jsonResult[0]['notable']['name'], 'wikilink': ''}
+			else:
+				return None
 		else:
 			return None
 	except Exception as e:
+		print('Error in Freebase Meaning ' + str(term) + ' : ' + str(e))
 		return None
 
 if __name__=="__main__":	
@@ -157,6 +207,10 @@ if __name__=="__main__":
 	categories = get_Text_Categories(text)
 	yahooapires = get_Content_Analysis(text)
 	calaisapires = get_Calais_Topics(text)
+	topiatermextract = get_topia_termextract(text)
+	nltkne = get_nltk_ne(text)
+	pythontagger = get_python_tagger(text)
+
 
 
 
