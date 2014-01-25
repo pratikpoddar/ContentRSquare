fa213eb11d9fd69017dfdd03e90b3ddcdb1f141d
Pratik Poddar <pratik.phodu@gmail.com>
2014-01-25 18:13:47 +0530

    Minor - Topic Modeling

--- crsq/crsqlib/text_summarize/text_topic_brown.py
+++ crsq/crsqlib/text_summarize/text_topic_brown.py
@@ -32,41 +32,31 @@ except Exception as e:
 	vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
 
-	data_train = fetch_20newsgroups(subset='train', categories=['rec.sport.baseball'],
-                                shuffle=True, random_state=42,
-                                remove=())
+	target_names = brown.categories()
+	data_train = []
+	data_train_target = []
 
-	data_train.data = []
-	data_train.target_names = brown.categories()
-	data_train.target = []
-	
 	documents = [(list(brown.words(fileid)), category)
 		for category in brown.categories()
 	       for fileid in brown.fileids(category)]
 
 	for doc in documents:
-		data_train.data += [' '.join(doc[0])]
-		data_train.target += [data_train.target_names.index(doc[1])]
+		data_train += [' '.join(doc[0])]
+		data_train_target += [target_names.index(doc[1])]
 		
-	y_train = data_train.target
-	X_train = vectorizer.fit_transform(data_train.data)
-	target_names = data_train.target_names
+	y_train = data_train_target
+	X_train = vectorizer.fit_transform(data_train)
 	clf = SGDClassifier(alpha=.0001, n_iter=50, penalty="l2")
 	clf.fit(X_train, y_train)
         with open('topic_classification_brown.pickle', 'w') as f:
 		pickle.dump([clf, vectorizer, target_names], f)
 
-data_test = fetch_20newsgroups(subset='test', categories=['rec.sport.baseball'],
-                               shuffle=True, random_state=42,
-                               remove=())
-data_test.data = ["Sachin Tendulkar is a great cricketeer. Sachin Ramesh Tendulkar (born 24 April 1973) is an Indian former cricketer widely acknowledged as the greatest batsman of the modern generation, popularly holds the title 'God of Cricket' among his fans. He is also acknowledged as the greatest cricketer of all time. He took up cricket at the age of eleven, made his Test debut against Pakistan at the age of sixteen, and went on to represent Mumbai domestically and India internationally for close to twenty-four years. He is the only player to have scored one hundred international centuries, the first batsman to score a Double Century in a One Day International, and the only player to complete more than 30,000 runs in international cricket.[10] In October 2013, he became the 16th player and first Indian to aggregate 50,000 runs in all recognized cricket (First-class, List A and Twenty20 combined). In 2002, Wisden Cricketers' Almanack ranked him the second greatest Test batsman of all time, behind Don Bradman, and the second greatest ODI batsman of all time, behind Viv Richards.[14] Later in his career, Tendulkar was a part of the Indian team that won the 2011 World Cup, his first win in six World Cup appearances for India.[15] He had previously been named 'Player of the Tournament' at the 2003 edition of the tournament, held in South Africa. In 2013, he was the only Indian cricketer included in an all-time Test World XI named to mark the 150th anniversary of Wisden Cricketers' Almanack"]
+text_data = ["Sachin Tendulkar is a great cricketeer. Sachin Ramesh Tendulkar (born 24 April 1973) is an Indian former cricketer widely acknowledged as the greatest batsman of the modern generation, popularly holds the title 'God of Cricket' among his fans. He is also acknowledged as the greatest cricketer of all time. He took up cricket at the age of eleven, made his Test debut against Pakistan at the age of sixteen, and went on to represent Mumbai domestically and India internationally for close to twenty-four years. He is the only player to have scored one hundred international centuries, the first batsman to score a Double Century in a One Day International, and the only player to complete more than 30,000 runs in international cricket.[10] In October 2013, he became the 16th player and first Indian to aggregate 50,000 runs in all recognized cricket (First-class, List A and Twenty20 combined). In 2002, Wisden Cricketers' Almanack ranked him the second greatest Test batsman of all time, behind Don Bradman, and the second greatest ODI batsman of all time, behind Viv Richards.[14] Later in his career, Tendulkar was a part of the Indian team that won the 2011 World Cup, his first win in six World Cup appearances for India.[15] He had previously been named 'Player of the Tournament' at the 2003 edition of the tournament, held in South Africa. In 2013, he was the only Indian cricketer included in an all-time Test World XI named to mark the 150th anniversary of Wisden Cricketers' Almanack"]
 
-data_test.data = ["No matter what has happened. No matter what you have done. No matter what you will do. I will always love you. I swear it. I wanted to tell you that wherever I am, whatever happens, I will always think of you, and the time we spent together, as my happiest time. I would do it all over again, if I had the choice. No regrets. Its one thing to fall in love. Its another to feel someone else fall in love with you, and to feel a responsibility toward that love."]
+text_data = ["No matter what has happened. No matter what you have done. No matter what you will do. I will always love you. I swear it. I wanted to tell you that wherever I am, whatever happens, I will always think of you, and the time we spent together, as my happiest time. I would do it all over again, if I had the choice. No regrets. Its one thing to fall in love. Its another to feel someone else fall in love with you, and to feel a responsibility toward that love."]
 
-data_test.target = None
-X_test = vectorizer.transform(data_test.data)
+X_test = vectorizer.transform(text_data)
 res = clf.predict(X_test)
 print(target_names[res])
-print(target_names)
 
 
