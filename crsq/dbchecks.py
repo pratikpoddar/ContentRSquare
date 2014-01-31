3ffbf83db427004277bc404e4f77ac1a76934b47
Pratik Poddar <pratik.phodu@gmail.com>
2014-01-31 05:14:58 +0000

    Dbchecks

--- crsq/dbchecks.py
+++ crsq/dbchecks.py
@@ -1,5 +1,6 @@
 from crsq.models import *
 from crsq.crsqlib import urlutils
+from urlparse import urlparse
 
 l1 = map(lambda x: x['url'], ArticleInfo.objects.all().values('url'))
 l2 = map(lambda x: x['url'], TweetLinks.objects.all().values('url'))
@@ -30,7 +31,7 @@ print "Links in TweetLinks that are short links"
 l8 = filter(lambda x: urlutils.isShortUrlPossibly(x), list(set(l2)))
 print l8
 print "Links in ArticleInfo that do not have date"
-l9 = map(lambda x: x['url'], ArticleInfo.objects.filter(articledate=None).filter(articledate='').values('url'))
+l9 = map(lambda x: x['url'], ArticleInfo.objects.filter(articledate=None).values('url'))
 print l9
 print "All Domains in TweetLinks"
 l10 = list(set(map(lambda x: urlparse(x)[1], l2)))
