4db08206faaa1bc24911727db5655f15c1e5cffb
Pratik Poddar <pratik.phodu@gmail.com>
2014-02-05 18:26:00 +0000

    Minor

--- crsq/twitter_newspaper/twitter_newspaper.py
+++ crsq/twitter_newspaper/twitter_newspaper.py
@@ -24,7 +24,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-twitter_newspaper_blocked_domains = ['thehill.com', 'shortyawards.com', 'ciowhitepapers.com', 'docquery.fec.gov', 'zao.com', 'wsj.com', 'blogs.wsj.com', 'on.wsj.com',  'online.wsj.com', 'thereformedbroker.com', 'indiadigitalreview.com']
+twitter_newspaper_blocked_domains = ['thehill.com', 'shortyawards.com', 'ciowhitepapers.com', 'docquery.fec.gov', 'zao.com', 'wsj.com', 'blogs.wsj.com', 'on.wsj.com',  'online.wsj.com', 'thereformedbroker.com', 'indiadigitalreview.com', 'jwatch.org']
 
 def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
 
