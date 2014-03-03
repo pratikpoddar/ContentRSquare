0fe535245a2d501489572a082a066b79b276dfea
Pratik Poddar <pratik.phodu@gmail.com>
2014-03-03 17:40:54 +0530

    Minor

--- crsq/dbchecks.py
+++ crsq/dbchecks.py
@@ -9,28 +9,28 @@ l1 = map(lambda x: x['url'], ArticleInfo.objects.all().values('url'))
 l2 = map(lambda x: x['url'], TweetLinks.objects.all().values('url'))
 l3 = map(lambda x: x['url'], ArticleSemantics.objects.all().values('url'))
 l4 = map(lambda x: x['url'], ArticleTags.objects.all().values('url'))
-print "Links in TweetLinks but not in ArticleInfo"
+print "Links in TweetLinks but not in ArticleInfo set(l2)-set(l1)"
 print set(l2)-set(l1)
-print "Links in ArticleInfo but not in ArticleSemantics"
+print "Links in ArticleInfo but not in ArticleSemantics set(l1)-set(l3)"
 print set(l1)-set(l3)
-#print "Links in ArticleInfo but not in ArticleTags"
+#print "Links in ArticleInfo but not in ArticleTags set(l1)-set(l4)"
 #print set(l1)-set(l4)
-print "Links in ArticleSemantics but not in ArticleInfo"
+print "Links in ArticleSemantics but not in ArticleInfo set(l3)-set(l1)"
 print set(l3)-set(l1)
-print "Links in ArticleTags but not in ArticleInfo"
+print "Links in ArticleTags but not in ArticleInfo set(l4)-set(l1)"
 print set(l4)-set(l1)
 print "Average Number of Tags for a Link"
 print len(l4) / len(set(l4))
-print "Links in ArticleInfo that are not articles"
+print "Links in ArticleInfo that are not articles l5"
 l5 = filter(lambda x: not urlutils.is_url_an_article(x), list(set(l1)))
 print l5
-print "Links in TweetLinks that are not articles"
+print "Links in TweetLinks that are not articles l6"
 l6 = filter(lambda x: not urlutils.is_url_an_article(x), list(set(l2)))
 print l6
-print "Links in ArticleInfo that are short links"
+print "Links in ArticleInfo that are short links l7"
 l7 = filter(lambda x: len(x)<30, filter(lambda x: urlutils.isShortUrlPossibly(x), list(set(l1))))
 print l7
-print "Links in TweetLinks that are short links"
+print "Links in TweetLinks that are short links l8"
 l8 = filter(lambda x: len(x)<30, filter(lambda x: urlutils.isShortUrlPossibly(x), list(set(l2))))
 print l8
 #print "Links in ArticleInfo that do not have date"
@@ -39,6 +39,7 @@ print l8
 #print "All Domains in TweetLinks"
 #l10 = list(set(map(lambda x: urlparse(x)[1], l2)))
 #print l10
+print "Duplicate urls tobedeleted"
 co = map(lambda x: x['articlecontent'], ArticleInfo.objects.all().values('articlecontent'))
 import collections
 dup_co = [x for x, y in collections.Counter(co).items() if y > 1]
@@ -52,8 +53,10 @@ for urlgroup in urlgroups:
 print tobedeleted
 l11 = map(lambda x: x['url'], ArticleInfo.objects.filter(url__contains="/~r/").values('url'))
 l12 = map(lambda x: x['url'], ArticleInfo.objects.filter(url__contains="http://feed").values('url'))
-print "ArticleInfo Links which are almost surely there by mistake"
+print "ArticleInfo Links which are almost surely there by mistake l11+l12"
 print l11+l12
-
+l13 = map(lambda x: x['url'], filter(lambda x: len(x['articlecontent'])<200, ArticleInfo.objects.all().values('articlecontent','url')))
+print "Too short Articles l13"
+print l13
 
 
