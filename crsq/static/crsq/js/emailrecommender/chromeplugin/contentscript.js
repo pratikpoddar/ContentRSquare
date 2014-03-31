62b2ee831368c0cc5ab4ad32321edeb8b8c2b880
Pratik Poddar <pratik.phodu@gmail.com>
2014-03-31 20:15:12 +0530

    Minor

--- crsq/static/crsq/js/emailrecommender/chromeplugin/contentscript.js
+++ crsq/static/crsq/js/emailrecommender/chromeplugin/contentscript.js
@@ -1,5 +1,5 @@
 var s = document.createElement('script');
-s.setAttribute('src', 'https://46.137.209.142/static/crsq/js/emailrecommender/crsqgmail.js?v=4.01');
+s.setAttribute('src', 'https://46.137.209.142/static/crsq/js/emailrecommender/crsqgmail.js?v=4.01212');
 (document.head||document.documentElement).appendChild(s);
 console.log('CRSQ Gmail Extractor Running')
 s.onload = function() {
