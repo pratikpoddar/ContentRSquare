5218e0044f3cecbb22957fe87840f15569d22ccf
Pratik Poddar <pratik.phodu@gmail.com>
2014-04-07 07:22:45 +0000

    Minor

--- crsq/feedanalyzer/run_feedanalyzer.py
+++ crsq/feedanalyzer/run_feedanalyzer.py
@@ -9,7 +9,6 @@ from django_cron import CronJobBase, Schedule
 logger = logging.getLogger(__name__)
 
 def fa():
-	#return
 	logger.debug('run_feedanalyzer - Getting Feed Result ')
 	feedextractor.load_rss_in_table("http://feeds.feedburner.com/TechCrunch/", "feed")
 	feedextractor.load_rss_in_table("http://feeds.feedburner.com/TechCrunch/startups", "feed")
