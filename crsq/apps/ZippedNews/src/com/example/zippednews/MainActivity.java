72808728bb93ee1b0abd505038716a923b41ed27
Pratik Poddar <pratik.phodu@gmail.com>
2014-06-09 02:02:02 +0530

    Android App and Minor

--- crsq/apps/ZippedNews/src/com/example/zippednews/MainActivity.java
+++ crsq/apps/ZippedNews/src/com/example/zippednews/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends Activity {
     private class MyCustomWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (Uri.parse(url).getHost().contains("zippednews.com")) {
+            if ((Uri.parse(url).getHost().contains("zippednews.com")) || true) {
                 // This is my web site, so do not override; let my WebView load the page
             	if(isNetworkStatusAvialable (getApplicationContext())) { 
             		view.loadUrl(url);
@@ -73,10 +73,13 @@ public class MainActivity extends Activity {
             	}
                 return false;
             }
-            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
-            return true;
+            
+            else {
+            	// Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            	startActivity(intent);
+            	return true;
+            }
         }
         
         @Override
