fb6f74af7a30a32a7f0ebab6c0ca06f9f1e54829
Pratik Poddar <pratik.phodu@gmail.com>
2014-06-11 20:53:01 +0000

    Minor

--- crsq/crsqlib/goose/crawler.py
+++ crsq/crsqlib/goose/crawler.py
@@ -55,6 +55,7 @@ class Crawler(object):
 
         parse_candidate = self.get_parse_candidate(crawl_candidate)
         raw_html = self.get_html(crawl_candidate, parse_candidate)
+	raw_html = raw_html.replace('<br>', '<br> ').replace('<br >', '<br > ').replace('<br/>', '<br/> ').replace('<br />', '<br /> ')
 
         if raw_html is None:
             return article
