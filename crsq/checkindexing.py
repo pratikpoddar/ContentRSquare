abb3eac5b9be949b882034c07ff92833174eb0d8
Pratik Poddar <pratik.phodu@gmail.com>
2014-06-26 11:03:15 +0000

    Minor

--- crsq/checkindexing.py
+++
@@ -1,8 +0,0 @@
-from crsq.models import *
-from crsq.crsqlib.article_elastic_search import *
-
-urls = map(lambda x: x['url'], ArticleInfo.objects.all().values('url'))
-for url in urls:
-    indexurl(url)
-
-topterms = gettopterms()
