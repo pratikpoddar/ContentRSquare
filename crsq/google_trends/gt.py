39f0eaf51180e39a3d0a8266d957b0868965c003
Pratik Poddar <pratik.phodu@gmail.com>
2014-10-15 03:20:04 +0530

    Minor

--- crsq/google_trends/gt.py
+++ crsq/google_trends/gt.py
@@ -13,7 +13,7 @@ def get_top_trends(gurl):
 	url = gurl+"/trends/hottrends/atom/feed"
 	xml = urllib2.urlopen(url).read()
 	xml = BeautifulSoup(xml, features="xml")
-	return list(set(map(lambda x: slugify(crsq_unicode(x.title.text).replace('.com','').replace('.co.in','').replace('.org', '').replace('.',' ')), xml.find_all('item'))))
+	return list(set(map(lambda x: slugify(crsq_unicode(x.title.text).replace('.com',' ').replace('.co.in',' ').replace('.org', ' ').replace('www.',' ').replace('.',' ')), xml.find_all('item'))))
 
 def get_all_google_trends():
 	return {'India': get_top_trends("http://www.google.co.in"), 'US': get_top_trends("http://www.google.com"), 'UK': get_top_trends("http://www.google.co.uk"), 'Australia': get_top_trends("http://www.google.com.au"), 'Spain': get_top_trends("http://www.google.es"), 'Brazil': get_top_trends("http://www.google.com.br"), 'Italy': get_top_trends("http://www.google.it"), 'France': get_top_trends("http://www.google.fr")}
