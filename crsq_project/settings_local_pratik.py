2997d3ed597767a3024c3de3ce02e02f28d547b8
Pratik Poddar <pratik.phodu@gmail.com>
2014-12-15 23:39:57 +0530

    More structured

--- crsq_project/settings_local_pratik.py
+++ crsq_project/settings_local_pratik.py
@@ -3,6 +3,9 @@ from .settings import *
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+DEBUG = True
+TEMPLATE_DEBUG = True
+
 NLTK_DATA = '/home/gauri/nltk_data/'
 ES_LOGS = '/home/gauri/eslogs/'
 
