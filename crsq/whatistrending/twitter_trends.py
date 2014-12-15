f812a7f0477e5e16d41c45f0a733696402cac140
Pratik Poddar <pratik.phodu@gmail.com>
2014-12-15 18:09:04 +0530

    Twitter Stories

--- crsq/whatistrending/twitter_trends.py
+++ crsq/whatistrending/twitter_trends.py
@@ -10,6 +10,11 @@ import time
 from cookielib import CookieJar
 import logging
 from crsq.crsqlib.stringutils import *
+import collections
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.collocations import *
 
 consumer_key="1up7tIphvvw9AAkBnvbHTNMjc"
 consumer_secret="dvUGo4u0e3Iw9GieBzneE0mHy5noqi4t8rdQKXgogGPsFH8dcB"
@@ -75,6 +80,81 @@ def get_all_twitter_trends():
         output[location] = get_top_trends(location_name=location)
     return output
 
+def get_twitter_stories(topic):
+	twitter_text = ""
+	all_words = []
+	n=0
+	stop = stopwords.words('english')
+	keyword = 'Shah Rukh Khan'
+	for tweet in tweepy.Cursor(api.search, q=keyword, rpp=200, result_type="recent", include_entities=True, geocode="", lang="en").items():
+		n+=1
+		if n==600:
+			break
+		text_string = tweet.text
+		print text_string
+		twitter_text += text_string.encode('utf8')
+		twitter_text += '. '
 
-        
+	read_data = ""
+	read_data = unicode(twitter_text, errors='ignore')
+
+	n=0
+	stop = stopwords.words('english')
+	stop += ['rt', 'com', 'http', 'http://', '&amp;'] 
+	replace_words = ['?', '(', ')', '"', '\n', ':', ',', '.', '#', 'http', '//t', '//']
+	read_data = read_data.lower()
+	for i in replace_words:
+		read_data = read_data.replace(i, '')	
+
+	tokens = read_data.split(' ')
+	wnl = nltk.WordNetLemmatizer()
+	wnl_tokens = [wnl.lemmatize(t) for t in tokens]
+	words = [i for i in wnl_tokens if i not in stop]
+	words = [i for i in words if len(i)>2]
+
+	text = nltk.Text(words)
+	most_common = nltk.FreqDist(text).most_common(50)
+
+	tokens_bigram = re.split(r'\W+', read_data)
+	wnl = nltk.WordNetLemmatizer()
+	wnl_tokens = [wnl.lemmatize(t) for t in tokens_bigram]
+	words = [i for i in wnl_tokens if i not in stop]
+	words = [i for i in words if len(i)>2]
+
+	bigram_measures = nltk.collocations.BigramAssocMeasures()
+	word_fd = nltk.FreqDist(words)
+	bigram_fd = nltk.FreqDist(nltk.bigrams(words))
+	finder = BigramCollocationFinder(word_fd, bigram_fd)
+
+	bigrams = finder.nbest(bigram_measures.raw_freq, 30)
+
+	for word in most_common:
+		print word
+
+	print "-----"
+
+	for bigram in bigrams:
+		print bigram
+
+	print "-----"
+
+	all_bigrams = []
+	for word in most_common:
+		n=0
+		word_bigrams=[]
+		for bigram in bigrams:
+			if word[0] in list(bigram):
+				n+=1
+				word_bigrams+=bigram
+				all_bigrams+=bigram
+		if n>1:
+			# print a, m, "---", list(set(word_bigrams))
+			print word[0], "---", list(set(word_bigrams))
+			pass
+
+	print "-----------"
+
+	counter=collections.Counter(all_bigrams)
+	for word in sorted(counter.items(), key=lambda x: x[1], reverse=True):
+		print word
 
