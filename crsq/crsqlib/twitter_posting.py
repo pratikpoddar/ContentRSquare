13a645545008607b95d7f76246a5a012f15c129e
Pratik Poddar <pratik.phodu@gmail.com>
2014-12-25 04:23:01 +0000

    Minor

--- crsq/crsqlib/twitter_posting.py
+++ crsq/crsqlib/twitter_posting.py
@@ -12,7 +12,8 @@ api = tweepy.API(auth)
 
 def post_twitter_crsq(znlink, link, title, tags):
     tags = sorted(tags, key=lambda x: -len(x))
-    title = title[:100]
+    if len(title)>100:
+	title = title[:97]+"..."
     for tag in tags:
         if tag.replace('-',' ') in title.lower():
 	    tag = tag.replace('-',' ')
